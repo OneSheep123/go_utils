771ed6754d4d3f71eab0e6935fa073f3096aeefe
OneSheep123 <1468254393@qq.com>
2024-09-07 15:50:44 +0800

    logger: 封装一个logger包，用于格式话消息

---
+++ logger/nop_log.go
@@ -0,0 +1,23 @@
+package logger
+
+type NoOpLogger struct {
+}
+
+func (n *NoOpLogger) With(args ...Field) Logger {
+	return n
+}
+
+func NewNoOpLogger() Logger {
+	return &NoOpLogger{}
+}
+
+func (n *NoOpLogger) Debug(msg string, args ...Field) {}
+
+func (n *NoOpLogger) Info(msg string, args ...Field) {
+}
+
+func (n *NoOpLogger) Warn(msg string, args ...Field) {
+}
+
+func (n *NoOpLogger) Error(msg string, args ...Field) {
+}
