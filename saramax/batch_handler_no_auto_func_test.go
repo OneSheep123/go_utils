1b9df7282ac83f5c16e1166c4edc28871af5f058
chencanhua <canhuachen@xiaoe-tech.com>
2025-04-21 23:44:23 +0800

    feature: 更新单元测试方法

--- saramax/batch_handler_no_auto_func_test.go
+++ saramax/batch_handler_no_auto_func_test.go
@@ -3,6 +3,7 @@ package saramax
 import (
 	"github.com/IBM/sarama"
 	"testing"
+	"time"
 )
 
 func TestKafkaConsumerV2(t *testing.T) {
@@ -11,11 +12,18 @@ func TestKafkaConsumerV2(t *testing.T) {
 	config.Version, _ = sarama.ParseKafkaVersion("0.10.2.1")
 	config.Consumer.Offsets.Initial = sarama.OffsetOldest
 	config.Consumer.Offsets.AutoCommit.Enable = false
-
+	kafkaAddress := "172.16.3.14:9092"
+	consumerGroup := "cch_test_v2"
 	topic := "cch_test"
-	// 初始化消费者
-	consumer := NewKafkaConsumer([]string{"172.16.3.14:9092"}, []string{topic}, "cch_test_v2", 100, config)
+
+	testKafkaConsumer(t, config, kafkaAddress, consumerGroup, topic)
+
+}
+
+func testKafkaConsumer(t *testing.T, config *sarama.Config, kafkaAddress string, consumerGroup string, topic string) {
+	consumer := NewKafkaConsumer([]string{kafkaAddress}, []string{topic}, consumerGroup, 100, config)
 	consumer.StartConsume(func(msg *sarama.ConsumerMessage) error {
+		time.Sleep(10 * time.Second)
 		t.Log(string(msg.Value))
 		return nil
 	})
